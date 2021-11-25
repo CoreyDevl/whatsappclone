ece00ae2f3fd6a3d84a926b85c89596d1f7b6c0a
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-25 16:56:48 -0300

    problema de inportação transposto!
    feito adaptação do comando de importação do material-ui
    e depois disso foram feitas  configs de css na barra de pesquisa..  meu projeto ficará com algumas diferenças  com o do professor para que meu projeto tenha ainda mais semelhança com o whatsapp original
    entao resolvi mudar  cor de body e dos icones...  e também padding  e scala dos icones

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
 export default () => {
   return (
     <div className="app-windows">
       <div className="sidebar">
         <header>
-    header
+        <img className="header--avatar" src="https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg" alt="" />
+        <div className="header--buttons">
+          <div className="header--btn">
+            <DonutLargeIcon style={{color: '#54585B'}}/>
+          </div>
+          <div className="header--btn">
+            <ChatIcon style={{color: '#54585B'}}/>
+          </div>
+          <div className="header--btn">
+            <MoreVertIcon style={{color: '#54585B'}}/>
+          </div>
+        </div>
         </header>
         <div className="search">
-        search
+        <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: '#54585B'}}/>
+            <input type="search" placeholder="Pesquisar ou começar uma nova conversa" name="" id="" />
+        </div>
         </div>
         <div className="chatlist">
           chatlist
