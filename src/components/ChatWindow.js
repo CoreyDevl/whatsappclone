12707dfcc1b5d47beeed67b9d9d5ff1219be072b
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-27 15:59:48 -0300

    painel de emoji estruturado
    a seguir a logica de  aparição do painel

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import './ChatWindow.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
-
+import TagFacesIcon from '@material-ui/icons/TagFaces';
+import MicIcon from '@material-ui/icons/Mic';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import EmojiPicker from 'emoji-picker-react';
+import { Autorenew } from "@material-ui/icons";
 
 export default ()=>{
+
+  const [emojiOpen, setEmojiOpen] = useState(false)
+
+  const handleEmojiClick = () => {
+
+  }
+
   return(
     <div className="chatWindow">
       <div className="chatWindow--header">
@@ -27,11 +39,43 @@ export default ()=>{
       <div className="chatWindow--body">
       
       </div>
-      <div className="chatWindow--footer">
-
+      <div 
+      className="chatWindow--emojiArea" 
+      style={{height: emojiOpen ? '200px' : '0px'}}      
+      >
+        <EmojiPicker 
+          onEmojiClick={handleEmojiClick}
+          disableSearchBar
+          disableSkinTonePicker
+         
+        />
       </div>
- 
-      
+      <div className="chatWindow--footer">
+        <div className="chatWindow--pre">
+   
+                <div className="chatWindow--btn">
+                <CloseIcon /><TagFacesIcon />
+                </div>
+                <div className="chatWindow--btn">
+                  <AttachFileIcon />
+                </div>
+        </div>
+        <div className="chatWindow--inputArea">
+          <input className="chatWindow--input" type="text" name="" id="" placeholder="Digite uma mensagem"/>
+        </div>
+        <div className="chatWindow--pos">
+        <div className="chatWindow--btn">
+          <SendIcon id="sendBtn"/> 
+          <MicIcon></MicIcon>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+
+
+
+ 
+      
