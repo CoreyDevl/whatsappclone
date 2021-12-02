549846f3037b1007210214affd66d8e29b3aef78
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-12-02 14:59:06 -0300

    foram criadas as coleções no Firebase
    coleção users e  chats  foram inseridas no firebase.
    logica de inserção de dados dos usuários  criada no componente NewMessage.

--- src/Api.js
+++ src/Api.js
@@ -18,5 +18,46 @@ export default {
       name: u.name,
       avatar: u.avatar
     }, {merge:true});
+  },
+  getContactList: async (userId) => {
+    let list = [];
+
+    let results = await db.collection('users').get();
+    results.forEach(result=> {
+      let data = result.data();
+        //ABAIXO vamos pegar a lista de usuários no firebase (menos o meu -que é esse 'if')  dessa verificação - e por fora das chaves -(lin38) - retornar a lista.
+      if(result.id !== userId.id){
+        list.push({
+          id: result.id,
+          name: data.name,
+          avatar: data.avatar
+        })
+      }
+    })
+
+    return list;
+  },
+  addNewChat: async(user, user2) => {
+    let newChat = await db.collection('chats').add({
+      messages: [],
+      users: [user.id, user2.id]
+    });
+    db.collection('users').doc(user.id).update({
+      chats: firebase.firestore.FieldValue.arrayUnion({
+        chatId: newChat.id,
+        title: user2.name,
+        image: user2.avatar,
+        with: user2.id
+      })
+    })
+    db.collection('users').doc(user2.id).update({
+      chats: firebase.firestore.FieldValue.arrayUnion({
+        chatId: newChat.id,
+        title: user.name,
+        image: user.avatar,
+        with: user.id
+      })
+    })
+
   }
 };
--- src/components/NewChat.js
+++ src/components/NewChat.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './NewChat.css';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import Api from '../Api';
 
 export default ({user, chatList, show, setShow})=>{
 
-  const [list, setList] = useState([
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-    {id: 123, avatar:'https://kariktheme.com/demos/default/assets/imgs/avatars/avatar-1.jpg', name: 'gorduchinha'},
-  ]);
+  const [list, setList] = useState([]);
+  
+  useEffect(()=> {
+     const getList = async () => {
+        if(user !== null){
+          let results = await Api.getContactList(user.id)
+          setList(results);
+        }
+    }
+    getList();
+  },[user])
     
+  const addNewChat = async (user2) => {
+    await Api.addNewChat(user, user2)
+    
+    handleClose()
+  }
+
   const handleClose =()=> {
     setShow(false)
   }
@@ -27,7 +37,7 @@ export default ({user, chatList, show, setShow})=>{
       </div>
       <div className="newChat--list">
         {list.map((item, key)=>(
-         <div key={key} className="newChat--item">
+         <div onClick={()=> addNewChat(item)} key={key} className="newChat--item">
           <img className="newChat--itemAvatar" src={item.avatar} alt="" />
           <div className="newChat--itemName">{item.name}</div>
          </div>
